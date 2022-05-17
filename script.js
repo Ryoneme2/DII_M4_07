687894ce95ba4a6ca9a0b2fcf9b3ef335084e451
Ryoneme2 <worknarair6@gmail.com>
2022-05-17 14:31:40 +0700

    done while fibo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// fibonacci
 const num = 10;
 let fibo1 = 0;
 let fibo2 = 1;
@@ -15,4 +14,24 @@ if (num === 0) {
     fibo1 = tmp;
   }
   console.log(fibo2);
+}
+
+const numW = 10;
+let fibo1W = 0;
+let fibo2W = 1;
+let tmpW;
+
+if (numW === 0) {
+  console.log(0);
+} else if (numW === 1) {
+  console.log(1);
+} else {
+  let k = 0;
+  while (k < numW-2) {
+    tmpW = fibo2W;
+    fibo2W = fibo1W + fibo2W;
+    fibo1W = tmpW;
+    k++
+  }
+  console.log(fibo2W);
 }
