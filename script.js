32b68e04f4c4ba68bf9b7654ac77725b3b51c210
Ryoneme2 <worknarair6@gmail.com>
2022-05-17 18:29:34 +0700

    find prime number from 2 to N

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// check prime number
-const number = 19
-let count = 0
+const number = 57
+let count;
+
 for(let i = 2; i < number; i++) {
-  if(number % i === 0) {
-    count++
-    break
+  count = 0
+  for(let j = 2; j <= i; j++) {
+    if(i % j === 0) {
+      count++
+    }
   }
-}
-if(count === 0) {
-  console.log(number + ' is prime number')
-} else {
-  console.log(number + ' is not prime number')
+  if(count == 1) {
+    console.log(i, 'is a prime number')
+  }
 }
