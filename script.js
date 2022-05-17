a34aae0f44697be1edb2ecb9c9e6809339c82815
Ryoneme2 <worknarair6@gmail.com>
2022-05-17 14:51:25 +0700

    is Prime Number

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-const num = 10;
-let fibo1 = 0;
-let fibo2 = 1;
-let tmp;
-
-if (num === 0) {
-  console.log(0);
-} else if (num === 1) {
-  console.log(1);
-} else {
-  for (let i = 0; i < num-2; i++) {
-    tmp = fibo2;
-    fibo2 = fibo1 + fibo2;
-    fibo1 = tmp;
+// check prime number
+const number = 19
+let count = 0
+for(let i = 2; i < number; i++) {
+  if(number % i === 0) {
+    count++
+    break
   }
-  console.log(fibo2);
 }
-
-const numW = 10;
-let fibo1W = 0;
-let fibo2W = 1;
-let tmpW;
-
-if (numW === 0) {
-  console.log(0);
-} else if (numW === 1) {
-  console.log(1);
+if(count === 0) {
+  console.log(number + ' is prime number')
 } else {
-  let k = 0;
-  while (k < numW-2) {
-    tmpW = fibo2W;
-    fibo2W = fibo1W + fibo2W;
-    fibo1W = tmpW;
-    k++
-  }
-  console.log(fibo2W);
+  console.log(number + ' is not prime number')
 }
