3d02abfee2eee8b9102d2f328191312026887668
Ryoneme2 <worknarair6@gmail.com>
2022-05-18 12:22:27 +0700

    alert date by click date

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const ok = document.getElementById("ok");
+const showDate = document.getElementById("showDate");
 const newText = "not Ok";
 
 ok.addEventListener("click", () => {
   alert("index.html")
 });
+
+showDate.addEventListener("click", () => {
+  alert(new Date());
+}
+);
