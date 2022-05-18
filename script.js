44ac4404ec1399df85882bdd9c834e8e3485a8a4
Ryoneme2 <worknarair6@gmail.com>
2022-05-18 12:19:05 +0700

    set click alert

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const ok = document.getElementById("ok");
 const newText = "not Ok";
 
 ok.addEventListener("click", () => {
-  if(ok.innerHTML === "Ok") {
-    ok.innerHTML = newText;
-  } else {
-    ok.innerHTML = "Ok";
-  }
+  alert("index.html")
 });
